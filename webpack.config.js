e550ee2e27109f5d2a6b664aa032cc1d9329e57f
Yakirv <yakirv@gmail.com>
2025-05-14 18:10:34 +0300

    sd

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/template.html',
-            filename: 'index.html',
+            filename: 'template.html',
         }),
     ],
 
